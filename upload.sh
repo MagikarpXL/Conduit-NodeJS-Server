7dd12dd6b1a29328d278a16fde369c7f7bd6b71e
Thomas van de Wege <wege0014@hz.nl>
2016-09-20 09:28:43 +0200

    upload.sh now only uploads index.js

--- upload.sh
+++ upload.sh
@@ -5,4 +5,4 @@ LOCAL=/home/magikarpxl/Documents/nodejs/Conduit-NodeJS-Server
 # Don't forget to setup SSH keys (http://www.linuxproblem.org/art_9.html)
 echo 'Starting to transfer files...'
 scp -prq $LOCAL/index.js admin@192.168.11.2:/Conduit-NodeJS-Server/index.js
-echo 'Transfered files :D'
+echo 'Transfered index.js :D'
